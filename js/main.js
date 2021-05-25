b56a762122d8b361ba67e53d7bec13b84e8a45b4
solbon <nayrero@mail.ru>
2021-05-25 09:21:46 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,66 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+/*плавная прокрутка*/
+/*var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#navbar-example'
+});*/
+$("body").on('click', '[href*="#"]', function(e){
+  var fixed_offset = 100;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
+
+/*маска для формы*/
+$(function(){ 
+  $("#phone").mask("+7(999) 999-9999");
+});
+
+// Отправка данных на сервер
+function send(event, php){
+console.log("Отправка запроса");
+event.preventDefault ? event.preventDefault() : event.returnValue = false;
+var req = new XMLHttpRequest();
+req.open('POST', php, true);
+req.onload = function() {
+	if (req.status >= 200 && req.status < 400) {
+	json = JSON.parse(this.response); // Ебанный internet explorer 11
+    	console.log(json);
+        
+    	// ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+    	if (json.result == "success") {
+    		// Если сообщение отправлено
+    		alert("Сообщение отправлено");
+    	} else {
+    		// Если произошла ошибка
+    		alert("Ошибка. Сообщение не отправлено");
+    	}
+    // Если не удалось связаться с php файлом
+    } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
+
+// Если не удалось отправить запрос. Стоит блок на хостинге
+req.onerror = function() {alert("Ошибка отправки запроса");};
+req.send(new FormData(event.target));
+}
+
+/*vue*/
+var vm = new Vue({
+	el: '#app',
+	data: {
+		message: 'Разработано с использованием Bootstrap'
+	}
+});
